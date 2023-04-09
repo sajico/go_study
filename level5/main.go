1b8f617201ffde66ab9bf23d5281897ac3d7a13c
harukasu4523 <airgir23@gmail.com>
2023-04-09 22:50:51 +0900

    add registor mode function

--- level5/main.go
+++ level5/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,9 +14,89 @@ type Item struct {
 	Inventory int
 }
 
-func register_mode() {
-	item := make(map[string]*Item)
+func To_int(str string) (int, bool) {
+	var str_num string = ""
+	if str[0] == '-' {
+		return -1, false
+	}
+	for _, c := range str {
+		if '0' <= c && c <= '9' {
+			str_num = str_num + string(str_num)
+		} else {
+			return -2, false
+		}
+	}
 
+	ret, _ := strconv.Atoi(str_num)
+
+	return ret, true
+}
+
+func is_continue() bool {
+	fmt.Println("続けて入力する時は1を終了する場合は0を入力してください")
+	for {
+		fmt.Printf("1 or 0：")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		is_next := scanner.Text()
+		if is_next == "1" {
+			return true
+		} else if is_next == "0" {
+			return false
+		} else {
+			fmt.Println("1か0を入力してください")
+			continue
+		}
+	}
+}
+
+func register_mode() map[string]*Item {
+	item := make(map[string]*Item)
+	fmt.Println("登録モードです")
+
+	var input_name string = ""
+	var input_price int = 0
+	var input_inventory int = 0
+	var flag bool = false
+
+	for {
+		fmt.Println("商品名を入力してください")
+		fmt.Printf("商品名：")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input_name = scanner.Text()
+
+		fmt.Println("価格を入力してください")
+		fmt.Printf("価格：")
+		scanner = bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input_price, flag = To_int(scanner.Text())
+		if !flag {
+			fmt.Println("正の整数を入力してください")
+			continue
+		}
+
+		fmt.Println("在庫数を入力してください")
+		fmt.Printf("在庫数：")
+		scanner = bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input_inventory, flag = To_int(scanner.Text())
+		if !flag {
+			fmt.Println("正の整数を入力してください")
+			continue
+		}
+
+		item[input_name] = &Item{Name: input_name, Price: input_price, Inventory: input_inventory}
+
+		if is_continue() {
+			fmt.Println("次の商品を入力してください")
+			continue
+		} else {
+			break
+		}
+	}
+
+	return item
 }
 
 func main() {
@@ -31,10 +112,11 @@ func main() {
 		}
 
 		if input[0] == "登録" {
-			register_mode()
+			item := register_mode()
+			fmt.Println(item)
 			break
 		} else if input[0] == "購入" {
-			customer_mode()
+			// customer_mode(item)
 			break
 		} else {
 			fmt.Println("登録か購入を記入してください")
